b762821840b7624238e981a27e2d52e645987304
Romeo Lager <rlager@deloitte.com>
2024-04-09 14:01:46 -0700

    updated item list

--- public/react/components/ItemList.js
+++ public/react/components/ItemList.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Item } from './item';
 
 export const ItemList = ({ items }) => {
+	const [isClicked, setIsClick] = useState(false) 
+
+	const handleClick =() => {
+		setIsClicked(!isClicked)
+	}
+	
 	return <>
 		{
 			items.map((item, idx) => {
