8df298da93622479f810d9a910e4e6b63958dc6e
Casey Segura <caseyrseguramgmt@gmail.com>
2024-04-09 21:02:55 -0400

    click to see view

--- public/react/components/item.js
+++ public/react/components/item.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Item = (props) => {
+  const [isClicked, setIsClicked] = useState(false) 
+
+	const handleClick =() => {
+		setIsClicked(!isClicked)
+	}
 
   return (
   <>
+    <div onClick={handleClick} style={{cursor: 'pointer'}}>
     <h3>{props.item.name}</h3>
-    <img src={props.item.image} alt={props.item.name} />
+    </div>
+    {isClicked && (
+      <div>
+        <img src={props.item.image} alt={props.item.name} />
+        </div>
+      )}
   </>
   )
 } 
-
