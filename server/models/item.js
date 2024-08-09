412b5e4eea07d787ae8c67048701d3cc40204dfd
Anton Council <afcounciljr@gmail.com>
2024-08-09 10:23:13 -0700

    Added unit tests

--- server/models/item.js
+++ server/models/item.js
@@ -1,10 +1,10 @@
 // + This file was added which defines Item model for the database
 
 // + Import the necessary modules from the 'db' file
-const { db, Sequelize, DataTypes, Model } = require('../db');
+const { db, sequelize, DataTypes, Model } = require('../db');
 
 // + Define the Item model with either ints or strings for each field
-const Item = db.define('Item', {
+const Item = sequelize.define('Item', {
     Name: DataTypes.STRING, 
     Description: DataTypes.STRING, 
     Price: DataTypes.INTEGER, 
